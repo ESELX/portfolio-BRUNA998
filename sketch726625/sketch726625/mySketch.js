fe4b0af2bfe3be52b059bd64a7820707e5f980da
bruna998 <bruna5@live.com.pt>
2019-06-06 15:18:55 +0100

    processing

---
+++ sketch726625/sketch726625/mySketch.js
@@ -0,0 +1,66 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(30,40,150);
+}
+
+function draw() {
+
+	
+	stroke(255,255,204);
+	fill (240,230,140)
+	square(mouseX,mouseY, 300, 300);
+	
+	stroke(255,255,204);
+	fill (255,255,255)
+	ellipse(640, 310, 90, 90);
+	
+	
+		stroke(0,0,0);
+	fill (255,255,255)
+	ellipse(640, 310, 90, 90);
+	
+		stroke(0);
+	fill (255,255,255)
+	ellipse(640, 310, 80, 80);
+	
+	stroke(255,255,204);
+	fill (255,255,255)
+	ellipse(760, 310, 90, 90);
+	
+	stroke( 0,0,0);
+	fill (255,255,255)
+	ellipse(760, 310, 90, 90);
+	
+		stroke(0);
+	fill (255,255,255)
+	ellipse(760, 310, 80, 80);
+	
+		stroke(0,0,0);
+	fill (0,0,0)
+	ellipse(640, 310, 60, 60);
+	
+				stroke(255,255,255);
+	fill (255,255,255)
+	ellipse(630, 290, 20, 20);
+	
+						stroke(255,255,255);
+	fill (255,255,255)
+	ellipse(655, 330, 15, 15);
+	
+			stroke(0,0,0);
+	fill (0,0,0)
+	ellipse(760, 310, 60, 60);
+	
+					stroke(255,255,255);
+	fill (255,255,255)
+	ellipse(750, 290, 20, 20);
+	
+					stroke(255,255,255);
+	fill (255,255,255)
+	ellipse(775, 330, 15, 15);
+	
+	stroke(255,255,255);
+	fill (0,0,0)
+	rect(610,420,172,20);
+	
+}
